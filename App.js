7f52b489b0348f612dce108aaeb781c7739f1d11
tamana007 <tamana.efatwira1@gmail.com>
2023-10-20 12:41:59 -0700

    second

--- App.js
+++ App.js
@@ -1,32 +1,56 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  const[color,setColor]=useState(0);
-  const[error,setError]=useState(false);
-  const[list,setList]=useState([]);
+  //set user's input
+  const [color, setColor] = useState(0);
+  //set if this color exist or error
+  const [error, setError] = useState(false);
+  //set lists of library colors
+  const [list, setList] = useState([]);
 
-  function handleBtn(e){
+  function handleBtn(e) {
     e.preventDefault();
-   
-    const colors=new Values(color).all(10);
-    console.log(colors);
 
+    // :::::::::::::I use this library for purpose of checking if my input colors exist in my predefined colors library or Not:::::::::::::
+    try {
+      const colors = new Values(color).all(10);
+      setList(colors);
+      console.log(color);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   }
-  return <>
-  <section className='container'>
-    <h2>color generator project</h2>
-    <form onSubmit={handleBtn}>
-      <input placeholder='#f515025' type='text' value={color} onChange={(e)=>{setColor(e.target.value)}} />
-      <button className='btn' type='submit'>submit</button>
-    </form>
-  </section>
-  <section>
-    <p>Colors go here</p>
-  </section>
-  </>
+  return (
+    <>
+      <section className="container">
+        <h2>color generator project</h2>
+        <form onSubmit={handleBtn}>
+          <input
+            placeholder="#f515025"
+            className={`${error ? "error" : "null"}`}
+            type="text"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {/* <p>{color} hi</p> */}
+        {list.map((item, index) => {
+          return <SingleColor key={index} index={index} {...color} />;
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- SingleColor.js
+++ SingleColor.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb,weight,index}) => {
+  return <>
+  <h4 >colors</h4>
+  
+  
+  </>
 }
 
 export default SingleColor
