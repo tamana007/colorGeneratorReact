b8616e7eb73b858b84da08b881b3343fc55368e2
tamana007 <tamana.efatwira1@gmail.com>
2023-10-20 13:45:20 -0700

    finished

--- SingleColor.js
+++ SingleColor.js
@@ -5,6 +5,13 @@ const SingleColor = ({rgb,weight,index}) => {
   const [alert,setAler]=useState(false);
   const bcg=rgb.join(',');
 
+  useEffect(()=>{
+    const timing=setTimeout(()=>{setAler(false)},3000)
+  
+  return()=>clearTimeout(timing)}
+  ,[alert]
+  )
+
   return <>
   <article className={'color'} style={{backgroundColor:`rgb(${bcg})`}} onClick={()=>{
     setAler(true)
